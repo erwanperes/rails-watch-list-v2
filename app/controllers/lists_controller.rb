53ffb331549f7fd0323bfc808e1d57608fb58b8f
Erwan Peres <eperes@jebidouille.com>
2024-10-22 19:53:53 -0400

    correction flash notice

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -2,29 +2,29 @@ class ListsController < ApplicationController
 	def index
 		@lists = List.all
 	end
-	
+
 	def show
 		@list = List.find(params[:id])
 	end
-	
+
 	def new
 		@list = List.new
 	end
-	
+
 	def edit
 		@list = List.find(params[:id])
 	end
-	
+
 	def update
 		@list = List.find(params[:id])
-	
+
 		if @list.update(list_params)
 		  redirect_to @list, notice: 'La liste a été mise à jour.'
 		else
 		  render :edit, status: :unprocessable_entity
 		end
 	end
-	
+
 	def create
 		@list = List.new(list_params)
 		if @list.save
@@ -33,17 +33,17 @@ class ListsController < ApplicationController
 		  render :new, status: :unprocessable_entity
 		end
 	end
-	
+
 	def destroy
 		@list = List.find(params[:id])
 		@list.destroy
 		redirect_to lists_path, status: :see_other,
-		alert: "La liste a été supprimée"
+		notice: "La liste a été supprimée"
 
 	end
-	
+
 	private
-	
+
 	def list_params
 		params.require(:list).permit(:name, :image)
 	end
